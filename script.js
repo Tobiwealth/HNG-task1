0aa8bc65683cedaae406006f6d5338c1eb05e3a9
Oluwatobi Oloro <Oluwatobi Oloro>
2023-09-11 01:01:27 +0100

    HNG task 1

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const slackUserName = document.querySelector('[data-testid="slackUserName"]');
+    const slackDisplayImage = document.querySelector('[data-testid="slackDisplayImage"]');
+    const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
+    const myTrack = document.querySelector('[data-testid="myTrack"]');
+    const githubURL = document.querySelector('[data-testid="githubURL"]');
+
+
+    slackUserName.textContent = "Tobi";
+    slackDisplayImage.src = "./icon.jpg";
+    slackDisplayImage.alt = "Tobi";
+    currentDayOfTheWeek.textContent = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+    currentUTCTime.textContent = Date.now();
+    myTrack.textContent = "Frontend";
+    githubURL.href = "https://github.com/Tobiwealth";
+    githubURL.textContent = "GitHub Repository";
+});
